576115afe3f3b7c720eff14b053b89b2a156d7fa
lambao <luu.lam.dev@gmail.com>
2015-04-26 20:46:54 +0900

    Initial Commit

---
+++ ProgressDialog/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgressDialog
+//
+//  Created by LamBao on 4/26/15.
+//  Copyright (c) 2015 LamBao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ProgressDialogTests/ProgressDialogTests.m
@@ -0,0 +1,40 @@
+//
+//  ProgressDialogTests.m
+//  ProgressDialogTests
+//
+//  Created by LamBao on 4/26/15.
+//  Copyright (c) 2015 LamBao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ProgressDialogTests : XCTestCase
+
+@end
+
+@implementation ProgressDialogTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
